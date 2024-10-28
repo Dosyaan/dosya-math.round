9011213b760c8262be13f6f5444e676ef26f36c2
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-28 19:34:23 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+// console.log(Math.round(4.8))
+
+// console.log(Math.floor(4.8))
+
+// console.log(Math.ceil(4.8))
+
+// task - 1
+
+// num = (prompt("Введите число"));
+// console.log(Math.round(num))
+
+// task - 2
+
+// const num = [3.1, 2.7, 4.4, 5.5]
+// const arr = []
+
+// num.forEach((item) =>{
+//     arr.push(Math.round(item))
+// })
+
+// console.log(arr)
+
+// task - 3
+
+// num1 = +prompt("Введите первое число");
+// num2 = +prompt("Введите второе число");
+
+// let sum = num1 + num2
+// console.log(Math.round(sum))
+
+// task - 4
+
+// const num = [1.5, 2.5, 3.5, 4.5]
+// const mid = num.reduce((prev, item) =>{
+//     return prev + item
+// })
+
+// console.log(Math.round(mid))
+
+// task - 5
+
+// const price = [12.3, 45.7, 22.8, 19.9]
+// const round = price.map((item) => {
+//     return Math.round(item)
+// })
+
+// console.log(round)
+
+// task - 6
+
+// let num = +prompt();
+// let int = Math.round(num / 5) * 5
+
+// console.log(int)
+
+// task - 7
+
+// let num = +prompt()
+// let int = Math.round(num * 10) / 10
+
+// console.log(int)
+
+// task - 8
+
+// task - 9
+
+let num = +prompt()
+let int = Math.floor.Math.round((num % 1) * 10) / 10
+
+console.log(int)
